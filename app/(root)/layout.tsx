5b6c4e9c36dde9a0b716e5e1d770b2d6af6d78f8
Yuhao Lin <yuhao4399@gmail.com>
2025-07-10 15:09:57 -0600

    getting header and footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,3 +1,6 @@
+import Header from "@/components/share/header";
+import Footer from "@/components/ui/footer";
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -5,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <div className='flex h-screen flex-col'>
+      <Header />
       <main className='flex-1 wrapper'>{children}</main>
+      <Footer/>
     </div>
   );
 }
