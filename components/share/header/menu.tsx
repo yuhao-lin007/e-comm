6038437bf4b7947af69fcef2476d326e505fe43c
Yuhao Lin <yuhao4399@gmail.com>
2025-07-14 02:23:59 -0600

    user button & sign out

--- components/share/header/menu.tsx
+++ components/share/header/menu.tsx
@@ -1,58 +1,49 @@
 import { Button } from "@/components/ui/button";
 import {
-Sheet,
-SheetDescription,
-SheetTitle,
-SheetContent,
-SheetTrigger,
+  Sheet,
+  SheetDescription,
+  SheetTitle,
+  SheetContent,
+  SheetTrigger,
 } from "@/components/ui/sheet";
 import ModeToggle from "./mode-toggle";
 import Link from "next/link";
 
-import { EllipsisVertical, ShoppingCart, UserIcon } from "lucide-react";
-
+import { EllipsisVertical, ShoppingCart} from "lucide-react";
+import UserButton from "./user-button";
 const Menu = () => {
-    return ( <div className="flex justify-end gap-3">
-        <nav className="hidden md:flex w-full max-w-xs gap-1">
-            <ModeToggle/>
-          <Button asChild variant='ghost'>
-            <Link href='/cart'>
-              <ShoppingCart />
-              Cart
-            </Link>
-          </Button>
-          <Button asChild>
-            <Link href='/sign-in'>
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-
-        </nav>
-        <nav className="md:hidden">
+  return (
+    <div className="flex justify-end gap-3">
+      <nav className="hidden md:flex w-full max-w-xs gap-1">
+        <ModeToggle />
+        <Button asChild variant="ghost">
+          <Link href="/cart">
+            <ShoppingCart />
+            Cart
+          </Link>
+        </Button>
+        <UserButton />
+      </nav>
+      <nav className="md:hidden">
         <Sheet>
-            <SheetTrigger className="align-middle">
-                <EllipsisVertical/>
-            </SheetTrigger>
-            <SheetContent className="flex flex-col items-start">
-                <SheetTitle>Menu</SheetTitle>
-                <ModeToggle/>
-                <Button asChild variant='ghost'>
-                    <Link href='/cart'>
+          <SheetTrigger className="align-middle">
+            <EllipsisVertical />
+          </SheetTrigger>
+          <SheetContent className="flex flex-col items-start">
+            <SheetTitle>Menu</SheetTitle>
+            <ModeToggle />
+            <Button asChild variant="ghost">
+              <Link href="/cart">
                 <ShoppingCart /> Cart
               </Link>
-                </Button>
-            <Button asChild>
-            <Link href='/sign-in'>
-              <UserIcon />
-              Sign In
-            </Link>
-          </Button>
-                <SheetDescription></SheetDescription>
-            </SheetContent>
+            </Button>
+            <UserButton />
+            <SheetDescription></SheetDescription>
+          </SheetContent>
         </Sheet>
-        </nav>
-    </div> );
-}
- 
+      </nav>
+    </div>
+  );
+};
+
 export default Menu;
---
+++ components/share/header/user-button.tsx
@@ -0,0 +1,68 @@
+import Link from "next/link";
+import { auth } from "@/auth";
+import { signOutUser } from "@/lib/actions/user.action";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenuContent,
+  DropdownMenu,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { UserIcon } from "lucide-react";
+
+const UserButton = async () => {
+  const session = await auth();
+
+  if (!session) {
+    return(
+    <Button asChild>
+      <Link href="/sign-in">
+        <UserIcon /> Sign In
+      </Link>
+    </Button>);
+  }
+  const firstInitial = session.user?.name?.charAt(0).toUpperCase() ?? "?";
+  //const imgSrc = `https://avatar.vercel.sh/${session.user?.name}.svg?text=${firstInitial}`;
+  return (
+    <div className="flex gap-2 items-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <div className="flex items-center">
+            <Button
+              variant="ghost"
+              className="relative w-8 h-8 rounded-full ml-2 flex items-center justify-center bg-gray-300"
+            >
+              {firstInitial}
+            </Button>
+          </div>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-56" align="end" forceMount>
+          <DropdownMenuLabel className="font-normal">
+            <div className="flex flex-col space-y-1">
+              <p className="text-sm font-medium leading-none">
+                {session.user?.name}
+              </p>
+              <p className="text-xs leading-none text-muted-foreground">
+                {session.user?.email}
+              </p>
+            </div>
+          </DropdownMenuLabel>
+
+          <DropdownMenuItem className="p-0 mb-1">
+            <form action={signOutUser} className="w-full">
+              <Button
+                className="w-full py-4 px-2 h-4 justify-start"
+                variant="ghost"
+              >
+                Sign Out
+              </Button>
+            </form>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  );
+};
+
+export default UserButton;
