e6d00a09d822bfb4bb14f5f141d2f549b33f8688
Yuhao Lin <yuhao4399@gmail.com>
2025-07-14 17:53:01 -0600

    enhance password security

--- lib/validators.ts
+++ lib/validators.ts
@@ -24,12 +24,27 @@ export const signUpFormSchema = z
   .object({
     name: z.string().min(3, "Name must be at least 3 characters"),
     email: z.string().email("Invalid email address"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
-    confirmPassword: z
+    password: z
       .string()
-      .min(6, "Confirm Password must be at least 6 characters"),
+      .min(8, { message: "Password must be at least 8 characters" })
+      .max(20, { message: "Password must be at most 20 characters" })
+      .refine((password) => /[A-Z]/.test(password), {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .refine((password) => /[a-z]/.test(password), {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .refine((password) => /[0-9]/.test(password), {
+        message: "Password must contain at least one number",
+      })
+      .refine((password) => /[!@#$%^&*]/.test(password), {
+        message: "Password must contain at least one special character",
+      }),
+    confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords dont match",
+    message: "Passwords don't match",
     path: ["confirmPassword"],
   });
+
+
