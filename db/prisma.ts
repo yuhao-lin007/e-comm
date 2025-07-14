4cfce8be4a273beeaddb1be722e7adbe598cee6e
Yuhao Lin <yuhao4399@gmail.com>
2025-07-13 19:22:10 -0600

    ready for initial depoly

--- db/prisma.ts
+++ db/prisma.ts
@@ -1,7 +1,6 @@
 import { neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
-import { PrismaClient} from "@/lib/generated/prisma";
-
+import { PrismaClient } from "@prisma/client";
 import ws from "ws";
 
 // Sets up WebSocket connections, which enables Neon to use WebSocket communication.
--- db/seed.ts
+++ db/seed.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from "@/lib/generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import sampleData from "./sample-data";
 
-async function main(){
-const prisma = new PrismaClient();
-await prisma.product.deleteMany();
+async function main() {
+  const prisma = new PrismaClient();
+  await prisma.product.deleteMany();
 
-await prisma.product.createMany({data: sampleData.products})
-console.log("seeed succes")
+  await prisma.product.createMany({ data: sampleData.products });
+  console.log("seeed succes");
 }
 
 main();
