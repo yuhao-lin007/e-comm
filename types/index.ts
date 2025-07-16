a18fd65da1e39158af55cf4269fc30e3b1e01bd3
Yuhao Lin <yuhao4399@gmail.com>
2025-07-15 22:15:25 -0600

    fix cookie type error

--- types/index.ts
+++ types/index.ts
@@ -1,7 +1,10 @@
 import z from "zod";
-import { insertProductSchema } from "@/lib/validators";
+import { insertProductSchema, insertCartSchema, cartItemSchema } from "@/lib/validators";
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
   created: Date;
 };
+
+export type Cart = z.infer<typeof insertCartSchema>;
+export type CartItem = z.infer<typeof cartItemSchema>;
