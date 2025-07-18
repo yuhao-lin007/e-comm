4354e31140781b89ed37ed78b095c59dc511acf1
Yuhao Lin <yuhao4399@gmail.com>
2025-07-17 20:04:36 -0600

    remove duplicate checkout step

--- app/(root)/shipping-address/shipping-address-form.tsx
+++ app/(root)/shipping-address/shipping-address-form.tsx
@@ -58,7 +58,6 @@ const ShippingAddressFrom = ({
 
   return (
     <>
-      <CheckoutSteps current={1} />
       <div className="max-w-md mx-auto space-y-4">
         <h1 className="h2-bold mt-4">Shipping Address</h1>
         <p className="text-sm text-muted-foreground">
