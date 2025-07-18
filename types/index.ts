d0fe4162abb2166bb4bd15c6cd4c6d6992a5782f
Yuhao Lin <yuhao4399@gmail.com>
2025-07-17 22:05:51 -0600

    add order summary page

--- types/index.ts
+++ types/index.ts
@@ -1,5 +1,12 @@
 import z from "zod";
-import { insertProductSchema, insertCartSchema, cartItemSchema, shippingAddressSchema } from "@/lib/validators";
+import {
+  insertProductSchema,
+  insertCartSchema,
+  cartItemSchema,
+  shippingAddressSchema,
+  insertOrderItemSchema,
+  insertOrderSchema,
+} from "@/lib/validators";
 export type Product = z.infer<typeof insertProductSchema> & {
   id: string;
   rating: string;
@@ -9,3 +16,14 @@ export type Product = z.infer<typeof insertProductSchema> & {
 export type Cart = z.infer<typeof insertCartSchema>;
 export type CartItem = z.infer<typeof cartItemSchema>;
 export type ShippingAddress = z.infer<typeof shippingAddressSchema>;
+export type OrderItem = z.infer<typeof insertOrderItemSchema>;
+export type Order = z.infer<typeof insertOrderSchema> & {
+  id: string;
+  createdAt: Date;
+  isPaid: boolean;
+  paidAt: Date | null;
+  isDelivered: boolean;
+  deliveredAt: Date | null;
+  orderItems: OrderItem[];
+  user: { name: string; email: string };
+};
